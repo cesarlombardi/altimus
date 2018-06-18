3730c784b8ffadccb6cde75cac5ed17e3313cc2c
cesar <cesar.a.lombardi@gmail.com>
2018-06-18 16:15:06 -0300

    'alteracao_db'

--- src/main/resources/db/migration/V03__criar_e_registrar_lancamentos.sql
+++ src/main/resources/db/migration/V03__criar_e_registrar_lancamentos.sql
@@ -13,29 +13,4 @@ create table lancamento(
 ) engine=innodb default charset=utf8;
 
 insert into lancamento(descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
-values ('Salário', '2018-01-05', null, 2000, null, 'RECEITA', 5, 1 );
-
-insert into lancamento(descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
-values ('Salário', '2018-01-05', null, 3000, null, 'RECEITA', 5, 3 );
-
-insert into lancamento(descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
-values ('Café', '2018-01-12', null, 5, null, 'DESPESA', 10, 4 );
-
-insert into lancamento(descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
-values ('Salário', '2018-01-05', null, 1200, null, 'RECEITA', 5, 2 );
-
-insert into lancamento(descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
-values ('Cinema', '2018-01-08', null, 18, null, 'DESPESA', 1, 3 );
-
-insert into lancamento(descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
-values ('Almoço', '2018-01-05', null, 25, null, 'DESPESA', 2, 2 );
-
-insert into lancamento(descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
-values ('Roupas', '2018-01-07', null, 150, null, 'DESPESA', 5, 1 );
-
-insert into lancamento(descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
-values ('Mercado', '2018-01-07', null, 200, null, 'DESPESA', 3, 5 );
-
-insert into lancamento(descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) 
-values ('Medicamento', '2018-01-15', null, 12, null, 'DESPESA', 4, 7 );
- 
+values ('Teste', '2018-01-05', null, 2000, null, 'RECEITA', 5, 1 );
